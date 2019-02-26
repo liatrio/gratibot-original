4fa2d327e0c62e54d3089e69f68a4e2e9b1afcc6
Takumi Nishida <takumin@liatrio.com>
2019-02-26 10:51:33 -0800

    ENG-235 Hard Coded Emoji (#9)
    * Changed the emoji value that was hard coded in to an environmental variable
    
    * Added default to env var in case EMOJI does not exist

--- skills/help.js
+++ skills/help.js
@@ -3,6 +3,7 @@ Module for defining help command for Gratibot.
 */
 
 module.exports = function (controller) {
+  emoji = process.env.EMOJI || ":toast:"
   controller.hears(['help'], 'direct_message,direct_mention', (bot, message) => {
     bot.reply(message, {
       attachments: [
@@ -10,17 +11,17 @@ module.exports = function (controller) {
           color: '#36a64f',
           title: 'Gratibot Repo\n',
           title_link: 'https://github.com/liatrio/gratibot',
-          pretext: '\nHey there :wave: Here\'s some help with what :toast: can do.\n',
+          pretext: '\nHey there :wave: Here\'s some help with what ' + emoji + ' can do.\n',
           fields: [
             {
               title: 'Commands',
-              value: '*:toast: <@user>* - Specify a user and the amount of :toast:s you want to award them!\n'
-                           + 'Example: _<@user> have a :toast: for demonstrating #empathy by helping me learn :vim:._\n',
+              'value': '*' + emoji + ' \<@user\>* - Specify a user and the amount of ' + emoji + ' you want to award them!\n'
+                           + 'Example: _\<@user\> have a ' + emoji + ' for demonstrating #empathy by helping me learn :vim:._\n',
             },
             {
               title: 'Details',
-              value: '*#<keyword>* - Tag your message with *#* to say why they deserve your :toast:. Ex. #excellence, #energy, #empathy\n'
-                           + '*:toast: limit* - Every user has a max of 5 :toast:s they can give which reset daily.',
+              value: '*#<keyword>* - Tag your message with *#* to say why they deserve your ' + emoji + '. Ex. #excellence, #energy, #empathy\n'
+                           + '*' + emoji + ' limit* - Every user has a max of 5 ' + emoji + 's they can give which reset daily.',
             },
           ],
         },
