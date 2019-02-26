fa9178d545c2f3414bd0459af346066022c984e2
Jonathon Rudy <jrudy2@mail.csuchico.edu>
2019-02-26 14:16:26 -0800

    Refactoring Code Functions

--- libs/funcs.js
+++ libs/funcs.js
@@ -1,26 +1,22 @@
 module.exports = {
-    description: function(message, convo) {
-      const emoji = process.env.EMOJI || ':toast:';
+    trimLength: function(message, emoji)
+    {
       const removeEmoji = new RegExp(emoji, 'g');
       let fullMessage = message.event.text;
-      // Gets all users mentioned
-      const catchUsers = fullMessage.match(/<.*?>/g);
-      const uniqueUsers = [...new Set(catchUsers)];
       // Strips users _ emoji out of message
       fullMessage = fullMessage.replace(/(\s)+(<.*?>)|(<.*?>)(\s)+/g, '');
       fullMessage = fullMessage.replace(removeEmoji, '');
-      if (fullMessage.length < 40) {
-        convo.ask(`Why is ${uniqueUsers} deserving of ${emoji} ?`, (response, newConvo) => {
-          if (response.text.length < 40) {
-            newConvo.say(`Distributing ${emoji} requires a description greater than 40 characters. Please try again`);
-            newConvo.next();
-          } else {
-            newConvo.say(`Awesome! Giving ${emoji} to ${uniqueUsers}`);
-            newConvo.next();
-          }
-        });
-      } else {
-        convo.say(`Awesome, ${emoji} given to ${uniqueUsers}`);
-      }
+      return fullMessage;
+    },
+
+    getUsers: function(message)
+    {
+      let fullMessage = message.event.text;
+      const catchUsers = fullMessage.match(/<.*?>/g);
+      const uniqueUsers = [...new Set(catchUsers)];
+      return uniqueUsers;
     }
+
+
+
 }
--- skills/description.js
+++ skills/description.js
@@ -6,7 +6,24 @@ const desc = require('../libs/funcs.js');
 module.exports = function checkDescription(controller) {
   controller.hears(process.env.EMOJI || ':toast:', 'ambient', (bot, message) => {
     bot.startConversation(message, (err, convo) => {
-      desc.description(message, convo);
+      const emoji = process.env.EMOJI || ':toast:';
+      var temp = desc.trimLength(message, emoji);
+      var uniqueUser = desc.getUsers(message);
+      if (temp.length < 40) {
+        convo.ask(`Why is ${uniqueUser} deserving of ${emoji} ?`, (response, newConvo) => {
+          if (response.text.length < 40) {
+            newConvo.say(`Distributing ${emoji} requires a description greater than 40 characters. Please try again`);
+            newConvo.next();
+          } else {
+            newConvo.say(`Awesome! Giving ${emoji} to ${uniqueUsers}`);
+            newConvo.next();
+          }
+        });
+      } else {
+        convo.say(`Awesome, ${emoji} given to ${uniqueUsers}`);
+      }
+
+
     });
   });
 };
