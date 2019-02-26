33f224dd51d2f54f852925c75caf7faf8449a919
Jonathon Rudy <jrudy2@mail.csuchico.edu>
2019-02-26 14:23:31 -0800

    Fixing linting errors

--- skills/description.js
+++ skills/description.js
@@ -7,23 +7,21 @@ module.exports = function checkDescription(controller) {
   controller.hears(process.env.EMOJI || ':toast:', 'ambient', (bot, message) => {
     bot.startConversation(message, (err, convo) => {
       const emoji = process.env.EMOJI || ':toast:';
-      var temp = desc.trimLength(message, emoji);
-      var uniqueUser = desc.getUsers(message);
-      if (temp.length < 40) {
+      const trimmedMessage = desc.trimLength(message, emoji);
+      const uniqueUser = desc.getUsers(message);
+      if (trimmedMessage.length < 40) {
         convo.ask(`Why is ${uniqueUser} deserving of ${emoji} ?`, (response, newConvo) => {
           if (response.text.length < 40) {
             newConvo.say(`Distributing ${emoji} requires a description greater than 40 characters. Please try again`);
             newConvo.next();
           } else {
-            newConvo.say(`Awesome! Giving ${emoji} to ${uniqueUsers}`);
+            newConvo.say(`Awesome! Giving ${emoji} to ${uniqueUser}`);
             newConvo.next();
           }
         });
       } else {
-        convo.say(`Awesome, ${emoji} given to ${uniqueUsers}`);
+        convo.say(`Awesome, ${emoji} given to ${uniqueUser}`);
       }
-
-
     });
   });
 };
