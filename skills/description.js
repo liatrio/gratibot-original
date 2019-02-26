6b88674211fb63085f847af4eae3c8779dd04bed
Jonathon Rudy <jrudy2@mail.csuchico.edu>
2019-02-26 12:08:35 -0800

    Fixing linting issues

--- skills/description.js
+++ skills/description.js
@@ -2,49 +2,31 @@
  * Module for checking description length.
  */
 
-module.exports = function(controller) {
-
-    controller.hears(process.env.EMOJI || ':toast:','ambient', function(bot, message) {
-
-        bot.startConversation(message, function(err, convo) {
-
-            //console.log(message)
-            var emoji = process.env.EMOJI || ':toast:'
-            var remove_emoji = new RegExp(emoji, "g");
-
-            full_message = message.event.text;
-            //Gets all users mentioned
-            var catch_Users = full_message.match(/<.*?>/g);
-
-            let unique_Users = [...new Set(catch_Users)];
-
-            //Strips users _ emoji out of message
-            full_message = full_message.replace(/(\s)+(<.*?>)|(<.*?>)(\s)+/g,'');
-            full_message = full_message.replace(remove_emoji,'');
-
-            console.log(full_message)
-
-            if (full_message.length < 40)
-            {
-                convo.ask('Why is ' + unique_Users + ' deserving of ' + emoji +"?", function(response, convo) {
-                    if (response.text.length < 40)
-                    {
-                        convo.say('Distributing ' + emoji + ' requires a description greater than 40 characters. Please try again');
-                        convo.next();
-                    }
-                    else
-                    {
-                        convo.say('Awesome! Giving ' + emoji + " to " + unique_Users);
-                        convo.next();
-                    }
-                });
-            }
-            else
-            {
-                convo.say('Awesome, ' + emoji + ' given to ' + unique_Users);
-            }
+module.exports = function checkDescription(controller) {
+  controller.hears(process.env.EMOJI || ':toast:', 'ambient', (bot, message) => {
+    bot.startConversation(message, (err, convo) => {
+      const emoji = process.env.EMOJI || ':toast:';
+      const removeEmoji = new RegExp(emoji, 'g');
+      let fullMessage = message.event.text;
+      // Gets all users mentioned
+      const catchUsers = fullMessage.match(/<.*?>/g);
+      const uniqueUsers = [...new Set(catchUsers)];
+      // Strips users _ emoji out of message
+      fullMessage = fullMessage.replace(/(\s)+(<.*?>)|(<.*?>)(\s)+/g, '');
+      fullMessage = fullMessage.replace(removeEmoji, '');
+      if (fullMessage.length < 40) {
+        convo.ask(`Why is ${uniqueUsers} deserving of ${emoji} ?`, (response, newConvo) => {
+          if (response.text.length < 40) {
+            newConvo.say(`Distributing ${emoji} requires a description greater than 40 characters. Please try again`);
+            newConvo.next();
+          } else {
+            newConvo.say(`Awesome! Giving ${emoji} to ${uniqueUsers}`);
+            newConvo.next();
+          }
         });
-
+      } else {
+        convo.say(`Awesome, ${emoji} given to ${uniqueUsers}`);
+      }
     });
-
+  });
 };
