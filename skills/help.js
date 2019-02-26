4607f586ebfeacf69810247ca576577139fc0797
Takumi Nishida <tnishida1@mail.csuchico.edu>
2019-02-25 16:43:48 -0800

    Removing repeat code from sample_hears.js

--- skills/help.js
+++ skills/help.js
@@ -4,8 +4,6 @@ Module for defining help command for Gratibot.
 
 */
 
-var wordfilter = require('wordfilter');
-
 module.exports = function(controller) {
 
     /* Collect some very simple runtime stats for use in the uptime/debug command */
@@ -21,36 +19,6 @@ module.exports = function(controller) {
     controller.on('conversationStarted', function() {
         stats.convos++;
     });
-
-
-    controller.hears(['^uptime','^debug'], 'direct_message,direct_mention', function(bot, message) {
-
-        bot.createConversation(message, function(err, convo) {
-            if (!err) {
-                convo.setVar('uptime', formatUptime(process.uptime()));
-                convo.setVar('convos', stats.convos);
-                convo.setVar('triggers', stats.triggers);
-
-                convo.say('My main process has been online for {{vars.uptime}}. Since booting, I have heard {{vars.triggers}} triggers, and conducted {{vars.convos}} conversations.');
-                convo.activate();
-            }
-        });
-
-    });
-
-    controller.hears(['^say (.*)','^say'], 'direct_message,direct_mention', function(bot, message) {
-        if (message.match[1]) {
-
-            if (!wordfilter.blacklisted(message.match[1])) {
-                bot.reply(message, message.match[1]);
-            } else {
-                bot.reply(message, '_sigh_');
-            }
-        } else {
-            bot.reply(message, 'I will repeat whatever you say.')
-        }
-    });
-
 
     controller.hears(['help'], 'direct_message,direct_mention,ambient', (bot, message) => {
         bot.reply(message, {
@@ -76,33 +44,6 @@ module.exports = function(controller) {
           ],
         });
       });
-
-
-    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-    /* Utility function to format uptime */
-    function formatUptime(uptime) {
-        var unit = 'second';
-        if (uptime > 60) {
-            uptime = uptime / 60;
-            unit = 'minute';
-        }
-        if (uptime > 60) {
-            uptime = uptime / 60;
-            unit = 'hour';
-        }
-        if (uptime != 1) {
-            unit = unit + 's';
-        }
-
-        uptime = parseInt(uptime) + ' ' + unit;
-        return uptime;
-    }
-
 };
 
 
