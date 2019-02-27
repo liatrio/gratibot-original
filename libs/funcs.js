4133337b344a0f18d476658115df9e91d0f08c97
Jonathon Rudy <jrudy2@mail.csuchico.edu>
2019-02-27 10:23:24 -0800

    adding unit tests for library functions

--- libs/funcs.js
+++ libs/funcs.js
@@ -1,5 +1,5 @@
 module.exports = {
-    trimLength: function(message, emoji)
+    trimLength: function (message, emoji)
     {
       const removeEmoji = new RegExp(emoji, 'g');
       let fullMessage = message.text;
@@ -9,14 +9,30 @@ module.exports = {
       return fullMessage;
     },
 
-    getUsers: function(message)
+    getUsers: function (message)
     {
       let fullMessage = message.text;
       const catchUsers = fullMessage.match(/<.*?>/g);
       const uniqueUsers = [...new Set(catchUsers)];
       return uniqueUsers;
     }
-
-
-
 }
+
+function trimLength(message, emoji)
+    {
+      const removeEmoji = new RegExp(emoji, 'g');
+      let fullMessage = message;
+      // Strips users _ emoji out of message
+      fullMessage = fullMessage.replace(/(\s)+(<.*?>)|(<.*?>)(\s)+/g, '');
+      fullMessage = fullMessage.replace(removeEmoji, '');
+      return fullMessage;
+    };
+
+
+    function getUsers(message)
+    {
+      let fullMessage = message;
+      const catchUsers = fullMessage.match(/<.*?>/g);
+      const uniqueUsers = [...new Set(catchUsers)];
+      return uniqueUsers;
+    };
