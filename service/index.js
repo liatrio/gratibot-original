b791843f5a7dfb61166f2d127b1d97e50050fcf8
Chris Schreiber <chriss@liatrio.com>
2019-02-26 17:49:36 -0800

    update server method definitions to return Promises

--- service/index.js
+++ service/index.js
@@ -9,28 +9,30 @@ module.exports = {
   * @param {string} message Slack message
   * @param {string} channel Slack channel the message was posted in
   * @param {array} values List of Liatrio values taged in message (#excellence)
+  * @return Promise resolves to result from mongodb insert
   **/
   giveRecognition: function(recognizer, recognizee, message, channel, values) {
-
+    return Promise.resolve({}); // TODO replace with promise returned from mongodb query
   },
   /**
   * Count the number of recognitions given to a user
   *
   * @param {string} user Name of Slack user recognized
   * @param {int} days Number of days to calculate count for
-  * @return int
+  * @return Promise which resolves to result from mongodb count query
   **/
   countRecognitionsGiven: function(user, days) {
-    return 0;
+    return Promise.resolve({}); // TODO replace with promise returned from mongodb query
   },
   /**
   * Count the nubmer of recogintions given out by a user
   *
   * @param {string} user Name of Slack user recognizing others
   * @param {int} days Number of days to calculate count for
-  * @return int
+  * @return Promise which resolves to result from mongodb count query
   **/
   countRecognitionsReceived: function(user, days) {
+    return Promise.resolve({}); // TODO replace with promise returned from mongodb query
   },
 
   /**
@@ -38,9 +40,9 @@ module.exports = {
   * Score = (Number of recognitions given to user) - (Number of recognitions given to user)/(Distinct number of users that have recognized user)
   *
   * @param {int} days Number of days to calculate leaderboard for
-  * @return Array [{user: 'USERNAME', score: SCORE_VALUE}]
+  * @return Promise which resolves to leaderboard data. Array [{user: 'USERNAME', score: SCORE_VALUE}]
   **/
   getLeaderboard: function(days) {
-    return ;
+    return Promise.resolve([{user: 'scribbles', score: 1000000}]); // TODO replace with promise which resolves to leaderboard data
   }
 }
--- test/service/index.js
+++ test/service/index.js
@@ -4,22 +4,41 @@ const service = require('../../service');
 describe('service index', () => {
   describe('gives recognition', () => {
     it('not cause exception', () => {
-      expect(service.giveRecognition).to.not.throw();
+      expect(() => { service.giveRecognition('userA', 'userB', 'message', 'channel', ['value1', 'value2']); }).to.not.throw();
+    });
+
+    it('returns promise', () => {
+      expect(service.giveRecognition('userA', 'userB', 'message', 'channel', ['value1', 'value2'])).to.be.an.instanceof(Promise);
     });
   });
+
   describe('count recognitions given', () => {
     it('not cause exception', () => {
-      expect(service.countRecognitionsGiven).to.not.throw();
+      expect(() => { service.countRecognitionsGiven('user', 1); }).to.not.throw();
+    });
+
+    it('returns promise', () => {
+      expect(service.countRecognitionsGiven('user', 1)).to.be.an.instanceof(Promise);
     });
   });
+
   describe('count recognitions received', () => {
     it('not cause exception', () => {
-      expect(service.countRecognitionsReceived).to.not.throw();
+      expect(() => { service.countRecognitionsReceived('user', 1); }).to.not.throw();
+    });
+
+    it('returns promise', () => {
+      expect(service.countRecognitionsReceived('user', 1)).to.be.an.instanceof(Promise);
     });
   });
+
   describe('get leaderboard', () => {
     it('not cause exception', () => {
-      expect(service.getLeaderboard).to.not.throw();
+      expect(() => { service.getLeaderboard(30); }).to.not.throw();
+    });
+
+    it('returns promise', () => {
+      expect(service.getLeaderboard(30)).to.be.an.instanceof(Promise);
     });
   });
 });
