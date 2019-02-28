9fe5587bc324bba7af14ed1c3ac2ba8a0af5a5ea
Chris Schreiber <chriss@liatrio.com>
2019-02-28 15:15:13 -0800

    create indexes for recognitions collection

--- service/mongo.js
+++ service/mongo.js
@@ -1,12 +1,22 @@
 var monk = require('monk');
 
+// Connect to mongodb server
 const db = monk(process.env.mongodbUri);
 
 db.catch(function(err) {
   throw new Error(err)
 });
 
+// Initialize recognition collection
+const recognition = db.get('recognition');
+recognition.ensureIndex('recognizer');
+recognition.ensureIndex('recognizee');
+recognition.ensureIndex('timestamp');
+
+// Initialize redemption collection
+const redemption = db.get('redemption');
+
 module.exports = {
-  recognition: db.get('recognition'),
-  redemption: db.get('redemption')
+  recognition: recognition,
+  redemption: redemption,
 }
