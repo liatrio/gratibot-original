48ea474a8245f593cf5d9048cf7745eea8e2bbdb
Justin Bankes <jbankes1@gmail.com>
2019-02-28 20:47:44 -0500

    ENG-247 initial addition of documentDB database

---
+++ infrastructure/database.tf
@@ -0,0 +1,22 @@
+#
+# DocumentDB Setup
+#
+
+data "aws_kms_key" "gratibot" {
+  key_id = "alias/gratibot"
+}
+
+resource "aws_docdb_cluster" "gratibot" {
+  cluster_identifier = "gratibot-cluster"
+  master_username    = "${var.master_username}"
+  master_password    = "${var.master_password}"
+  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  storage_encrypted  = true
+  kms_key_id         = "${data.aws_kms_key.gratibot.arn}"
+}
+
+resource "aws_docdb_cluster_instance" "gratibot_cluster_instances" {
+  cluster_identifier = "${aws_docdb_cluster.gratibot.id}"
+  identifier         = "docdb-cluster-gratibot-${count.index}"
+  instance_class     = "db.r4.large"
+}
