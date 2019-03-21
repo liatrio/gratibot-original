5e8696fbec97d6f0a88fdde11aef36bf5ad83c07
Casey Lee <cplee@nektos.com>
2019-03-21 13:31:49 -0700

    increase mocha timeout

--- test/eslint.js
+++ test/eslint.js
@@ -17,7 +17,7 @@ const options = {
   alwaysWarn: false, // Defaults to `true`, always show warnings
 
   // Increase the timeout of the test if linting takes to long
-  timeout: 5000, // Defaults to the global mocha `timeout` option
+  timeout: 15000, // Defaults to the global mocha `timeout` option
 
   // Increase the time until a test is marked as slow
   slow: 1000, // Defaults to the global mocha `slow` option
