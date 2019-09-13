7ec4abf5333d71008a0b7275ef0dc825cecb8027
Sean Jordan <seanj@liatrio.com>
2019-09-13 12:40:32 -0700

    Removed some of those .catch's

--- service/mongo.js
+++ service/mongo.js
@@ -1,26 +1,23 @@
-var monk = require('monk');
+const monk = require('monk');
 
 // Connect to mongodb server
-const db = monk(process.env.mongodbUri).catch(err => {
+const db = monk(process.env.mongodbUri)
+
+db.then(() => {
+  console.log("Connected Properly");
+}).catch(err => {
   console.log("Failed to connect to the database");
   console.log(err);
 });
 
 // Initialize recognition collection
-const recognition = db.get('recognition').catch(err => {
-  console.log("Failed to get recognition");
-  console.log(err);
-});
+const recognition = db.get('recognition');
 recognition.ensureIndex('recognizer');
 recognition.ensureIndex('recognizee');
 recognition.ensureIndex('timestamp');
 
 // Initialize redemption collection
-const redemption = db.get('redemption').catch(err => {
-  console.log("Failed to get redemption");
-  console.log(err);
-});
-
+const redemption = db.get('redemption');
 
 module.exports = {
   recognition: recognition,
