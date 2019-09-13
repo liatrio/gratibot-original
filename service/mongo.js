809caed39f2e6d670bdc6df8fe6fd7090d891843
Sean Jordan <seanj@liatrio.com>
2019-09-13 12:26:01 -0700

    Added .catch blocks to everything

--- service/mongo.js
+++ service/mongo.js
@@ -1,22 +1,26 @@
 var monk = require('monk');
 
 // Connect to mongodb server
-const db = monk(process.env.mongodbUri);
-
-db.catch(function(err) {
-  console.log(process.env.mongodbUri);
-  console.log(process.env.clientId);
-  throw new Error(err)
+const db = monk(process.env.mongodbUri).catch(err => {
+  console.log("Failed to connect to the database");
+  console.log(err);
 });
 
 // Initialize recognition collection
-const recognition = db.get('recognition');
+const recognition = db.get('recognition').catch(err => {
+  console.log("Failed to get recognition");
+  console.log(err);
+});
 recognition.ensureIndex('recognizer');
 recognition.ensureIndex('recognizee');
 recognition.ensureIndex('timestamp');
 
 // Initialize redemption collection
-const redemption = db.get('redemption');
+const redemption = db.get('redemption').catch(err => {
+  console.log("Failed to get redemption");
+  console.log(err);
+});
+
 
 module.exports = {
   recognition: recognition,
